07cc4623d10d28b17077b0935a3a90969263174a
GabrielGHub <56793117+GabrielGHub@users.noreply.github.com>
2019-10-20 11:09:07 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send({ message: 'Olá mundo! Estou funcionando!'});
+app.get('/olá', (req, res) => {
+    res.status(200).send({ message: 'Oi, eu sou a rota Olá'});
 })
 
 app.listen(3001, () => {
